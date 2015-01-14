e5d2c74018c52097e6b6b196272a62b7ffc5eb0a
Anson Yu Wang <opensource@undozen.com>
2015-01-14 22:55:30 +0800

    ok

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gulp-factor-bundle
 a gulp plugin help you use factor-bundle
+
+## Options
+
+### alterPipeline: function alterPipeline(pipeline, b, lib)
+provide this function if you want to alter pipeline such as change bpack prelude output.
+
+### b: browserify object
+### basedir: usually `__dirname`, default: process.cwd()
+### commonJsPath:
+### threshould: function (row, group)
---
+++ index.js
@@ -0,0 +1,80 @@
+'use strict';
+var es = require('event-stream');
+var source = require('vinyl-source-stream2');
+var StreamQueue = require('streamqueue');
+var StreamCombiner = require('stream-combiner');
+var browserify = require('browserify');
+var path = require('path');
+var xtend = require('xtend');
+var through = require('through2');
+
+module.exports = function (opts) {
+    if (!opts) {
+        opts = {};
+    }
+    var cwd = process.cwd();
+    opts.base = opts.basedir || cwd;
+    var b = opts.b || browserify();
+    var cache = opts.cache || {};
+    opts.fullPaths = true;
+    opts.exposeAll = true;
+    b.on('dep', function (dep) {
+        if (typeof dep.id === 'string') {
+            cache[dep.id] = dep;
+        }
+    });
+    //b.reset(opts);
+
+    var vfiles = [];
+    var outStream = new StreamQueue({
+        objectMode: true
+    });
+    var inStream = through.obj(function write(vfile, enc, next) {
+        vfiles.push(vfile);
+        next(null);
+    }, function end(done) {
+        var ss = vfiles.map(function (vfile) {
+            var s = source({
+                cwd: vfile.cwd,
+                base: vfile.base,
+                path: vfile.path
+            });
+            s.on('error', function (err) {
+                console.error(err);
+                console.error(err.stack);
+                return done();
+            });
+            return s;
+        });
+        var s = source({
+            base: opts.base,
+            path: opts.commonJsPath[0] === '/' ? opts.commonJsPath : path.join(
+                opts.base, opts.commonJsPath),
+            cwd: cwd
+        });
+        var ess = es.merge.apply(null, ss.concat(s));
+        outStream.queue(ess);
+        outStream.done();
+        b.plugin('factor-bundle', xtend(opts, b._options, { outputs: ss }));
+        b.reset(opts);
+        if (typeof opts.alterPipeline === 'function') {
+            opts.alterPipeline(b.pipeline, b, true);
+            b.on('factor.pipeline', function (file, pipeline) {
+                opts.alterPipeline(pipeline, b, false);
+            });
+        }
+        b.add(vfiles.map(function (vfile) {
+            return vfile.path;
+        }));
+        b.bundle().pipe(s);
+        ess.on('error', function (err) {
+            console.error(err);
+            console.error(err.stack);
+            return done();
+        });
+        ess.on('end', function () {
+            return done();
+        });
+    });
+    return StreamCombiner(inStream, outStream);
+};
